193b6379616a43a598310e482c18463959518bf6
phuoctc <phuoctc@inmobivn.com>
2025-05-30 23:43:00 +0700

    first commit

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+// const api = axios.create({
+//     baseURL: window.location.hostname == "localhost" ?
+//             import.meta.env.VITE_DEV_URL : window.location.origin,
+// });
 const api = axios.create({
-    baseURL: window.location.hostname == "localhost" ?
-            import.meta.env.VITE_DEV_URL : window.location.origin,
+    baseURL: import.meta.env.VITE_DEV_URL
 });
    
 export default api;
